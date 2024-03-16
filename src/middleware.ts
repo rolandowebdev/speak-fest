875fa58c00278c22fa469c43743139372e396c1a
rolandowebdev <rolandopranata@gmail.com>
2024-03-16 19:19:32 +0700

    chore: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,43 @@
 import { NextResponse, NextRequest } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 
-export async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl
 
   const arrayOfPathname = pathname.split('/')
   const currentPathname = `/${arrayOfPathname[1]}`
 
-  const token = await getToken({
-    req,
+  // Check if the user is authenticated
+  const tokenAuth = await getToken({
+    req: request,
     secret: process.env.NEXTAUTH_SECRET,
   })
 
-  if (token) {
+  // If the token doesn't exist, redirect to the login page
+  if (!tokenAuth) {
+    if (
+      currentPathname.includes('/profile') ||
+      currentPathname.includes('/create-thread')
+    ) {
+      return NextResponse.redirect(new URL('/login', request.url))
+    }
+  }
+
+  // If the user is authenticated, allow access to other routes
+  if (tokenAuth) {
     if (
       currentPathname.includes('/login') ||
       currentPathname.includes('/register')
     ) {
-      return NextResponse.redirect(new URL('/', req.url))
+      return NextResponse.redirect(new URL('/', request.url))
     }
-  } else if (
-    currentPathname.includes('/profile') ||
-    currentPathname.includes('/create-thread')
-  ) {
-    return NextResponse.redirect(new URL('/', req.url))
+  }
+
+  // Redirect threads page to home if user not authenticated
+  const pathnameToThreads = ['/threads', '/threads/']
+
+  if (!tokenAuth && pathnameToThreads.includes(currentPathname)) {
+    return NextResponse.redirect(new URL('/', request.url))
   }
 
   return NextResponse.next()
