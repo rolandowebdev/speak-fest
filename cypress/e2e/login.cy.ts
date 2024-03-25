9859683b51fdd96f93d21fa5b7a4610b9d97b948
rolandowebdev <rolandopranata@gmail.com>
2024-03-26 00:45:24 +0700

    chore: increase timeout before redirect page after login

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -85,14 +85,6 @@ describe('Login spec', () => {
     cy.get('input[type="password"]').type('zeta123')
     cy.get('form').submit()
 
-    cy.url().then((url) => {
-      cy.log('Current URL:', url)
-    })
-
-    cy.url({ timeout: 60000 }).should('eq', 'http://localhost:3000/')
-
-    cy.url().then((url) => {
-      cy.log('Final URL:', url)
-    })
+    cy.url({ timeout: 10000 }).should('eq', 'http://localhost:3000/')
   })
 })
