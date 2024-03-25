f05e4b105097462281ba931dfb991c44c7ee7ee6
rolandowebdev <rolandopranata@gmail.com>
2024-03-26 05:36:51 +0700

    chore: remove config component dev server in cypress config

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -85,6 +85,6 @@ describe('Login spec', () => {
     cy.get('input[type="password"]').type('zeta123')
     cy.get('form').submit()
 
-    cy.url({ timeout: 10000 }).should('eq', 'http://localhost:3000/')
+    cy.url().should('eq', 'http://localhost:3000/')
   })
 })
